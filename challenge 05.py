dc02e971e1c00badd4693aaa2a44cc56a968c507
Chathurya Prasad <121708803+CPrasa@users.noreply.github.com>
2024-05-11 00:02:32 +0530

    leap year problem done

---
+++ challenge 05.py
@@ -0,0 +1,17 @@
+#write a function
+#leap year problem
+
+def is_leap(year):
+    leap = False
+    if year%400 == 0:
+        leap =True
+    elif year%100 != 0:
+        leap =False
+    elif year % 4 == 0:
+        leap =True
+    # Write your logic here
+    
+    return leap
+
+year = int(input())
+print(is_leap(year))
