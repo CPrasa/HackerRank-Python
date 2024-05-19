f66fa26f0ed7b4b16bf2e5cc03991b4b6d951fe5
Chathurya Prasad <121708803+CPrasa@users.noreply.github.com>
2024-05-19 23:33:33 +0530

    half done

---
+++ challenge 09.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+  arr=[] 
+  for _ in range(int(input())):
+    name = input()
+    score = float(input())
+    arr.append([name,score])
+    
+  
+  print()
+    
+    
+        
+        
